968d2625c1442f6546bdb2ce67101498de48fe2d
mbcole <mbeloc@gmail.com>
2017-12-14 02:17:47 -0800

    Gayo Data Added

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Data: Normalized RNA-Seq data and meta data for Gayo et al. Human DC Study
+#' 
+#' @details Normalized RSEM transcriptome abundance estimates for 393 cells
+#'   passing sample filter. Normalized RSEM log(TPM+1) estimates as described
+#'   in SOM of Gayo et al. Metadata and alignment quality control metrics for
+#'   RSEM transcriptome alignments are included in the SummarizedExperiment
+#'   object.
+#'   
+#' @details Average normalized RSEM transcriptome abundance estimates for 5
+#'   human bulk TLR perturbations. Normalized RSEM log(TPM+1) estimates as
+#'   described in SOM of Gayo et al.
+#'   
+#' @name gayoData
+#'   
+#' @docType data
+#' @aliases gayoSCDC gayoBulkTLR
+#'   
+#' @examples
+#' data(gayoSCDC)
+#' data(gayoBulkTLR)
+NULL
